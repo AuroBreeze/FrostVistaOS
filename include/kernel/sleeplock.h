f38ea80eaba80aa0e4c8f75735d96ea644589268
AuroBreeze <1732373074@aliyun.com>
2026-04-15 16:47:14 +0800

    feat: Add sleeplock

---
+++ include/kernel/sleeplock.h
@@ -0,0 +1,14 @@
+#ifndef __KERNEL_SLEEPLOCK_H__
+#define __KERNEL_SLEEPLOCK_H__
+
+#include "kernel/spinlock.h"
+
+struct sleeplock {
+  int locked;
+  struct spinlock lock;
+
+  char *name;
+  int pid;
+};
+
+#endif
