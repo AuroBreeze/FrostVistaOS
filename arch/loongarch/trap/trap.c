6070e731c66b2f381b7f4c51f9306084b2757f54
AuroBreeze <1732373074@aliyun.com>
2026-08-25 08:40:36 +0800

    feat(loongarch): add early trap bring-up scaffold
    - define the initial trap frame and exception status helpers
    
    - install the early kernel trap vector
    
    - save the minimal state needed by the current bring-up path
    
    - report exception and interrupt state over UART
    
    - retain the early boot exception trigger for experimentation

--- arch/loongarch/trap/trap.c
+++ arch/loongarch/trap/trap.c
@@ -1,5 +1,21 @@
-void Eentry(void)
+#include "asm/trap.h"
+#include "asm/loongarch.h"
+#include "kernel/types.h"
+#include "platform/uart.h"
+
+void trap_handler(void)
 {
+	uart_puts("entry trap\n");
+	uint64 estat = r_estat();
+	if (is_interrupt(estat)) {
+		uint64 badv = r_badv();
+		kprintf("badv: %x\n", badv);
+		kprintf("Interrupt\n");
+	} else {
+		kprintf("Exception\n");
+	}
+
+	uart_puts("test\n");
 	for (;;)
 		;
 }
