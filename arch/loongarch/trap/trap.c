dbaa1226cb1d633636d3a29451137eddf9523148
AuroBreeze <1732373074@aliyun.com>
2026-08-26 11:25:13 +0800

    feat(loongarch-boot): wire early memory initialization
    - initialize the architecture allocator during early boot
    - enable the LoongArch page-table bring-up path
    - improve early exception diagnostics over the console

--- arch/loongarch/trap/trap.c
+++ arch/loongarch/trap/trap.c
@@ -1,14 +1,14 @@
 #include "asm/trap.h"
 #include "asm/loongarch.h"
-#include "kernel/types.h"
 #include "kernel/log.h"
+#include "kernel/types.h"
 
 #define ESTAT_IS_TIMER (1ULL << 11)
 
 void trap_handler(void)
 {
 	// kprintf("Entry trap\n");
-	LOG_TRACE("Entry trap");
+	kprintf("Entry trap\n");
 
 	uint64 estat = r_estat();
 	if (is_interrupt(estat)) {
@@ -18,19 +18,19 @@ void trap_handler(void)
 			kprintf("Timer\n");
 			return;
 		}
-		kprintf("is: %x\n", is);
+		kprintf("is: 0x%x\n", is);
 		kprintf("Interrupt\n");
 	} else {
 		uint64 ecode = estat_ecode(estat);
 		if (ecode == 0x8) {
 			uint64 esubcode = estat_esubcode(estat);
-			kprintf("esubcode: %x\n", esubcode);
+			kprintf("esubcode: 0x%x\n", esubcode);
 		}
 		if (ecode == -0xB) {
 			w_era(r_era() + 4);
 		}
 
-		kprintf("ecode: %x\n", ecode);
+		kprintf("ecode: 0x%x\n", ecode);
 		kprintf("Exception\n");
 	}
 
